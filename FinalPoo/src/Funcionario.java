ddc197302a018eb3aa7001cb2678f2a5a1977a95
Victor-ChBo <victor.bolanos@a.ucb.br>
2025-11-28 16:10:26 -0300

    Add files via upload

---
+++ FinalPoo/src/Funcionario.java
@@ -0,0 +1,45 @@
+public class Funcionario extends Pessoa {
+    private String cargo;
+    private double salario;
+
+    // Construtor
+    public Funcionario(String nome, String cpf, String email, String cargo, double salario) {
+        super(nome, cpf, email);
+        this.cargo = cargo;
+        this.salario = salario;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public void exibirInfo() {
+        System.out.println("Nome: " + getNome());
+        System.out.println("CPF: " + getCpf());
+        System.out.println("Cargo: " + cargo);
+        System.out.println("Salário: " + salario);
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionario{" +
+                "nome='" + getNome() + '\'' +
+                ", cpf='" + getCpf() + '\'' +
+                ", email='" + getEmail() + '\'' +
+                ", cargo='" + cargo + '\'' +
+                ", salario=" + salario +
+                '}';
+    }
+}
